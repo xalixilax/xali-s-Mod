a22d69d3f4cc8933fac0268b9a42b5b6b50d0ac5
Maxime L <101675927+LefOMax@users.noreply.github.com>
2022-08-24 11:14:33 -0400

    Initial Commit

---
+++ src/main/java/net/xalixilax/untitledmod/UntitledMod.java
@@ -0,0 +1,14 @@
+package net.xalixilax.untitledmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UntitledMod implements ModInitializer {
+	public static final String MOD_ID = "untitledmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/xalixilax/untitledmod/UntitledModClient.java
@@ -0,0 +1,10 @@
+package net.xalixilax.untitledmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UntitledModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
